9f4e678d3ecd78992ce0b1fedc04d99b13965d91
Riley Snyder <riley.snyder@harness.io>
2024-06-25 11:19:58 -0500

    add docstrings

--- plugin.py
+++ plugin.py
@@ -5,6 +5,13 @@ from requests import get
 
 
 def write_outputs(outputs: dict[str, str]):
+    """
+    write key value outputs to a local file to be rendered in the plugin step
+
+    args:
+        outputs (dict[str, str]): string to string mappings
+    """
+
     output_file = open(getenv("DRONE_OUTPUT"), "a")
 
     for k, v in outputs.items():
@@ -13,12 +20,32 @@ def write_outputs(outputs: dict[str, str]):
     output_file.close()
 
 
-def main():
-    url = getenv("PLUGIN_URL")
+def check_env(variable: str, default: str = None):
+    """
+    resolves an environment variable, returning a default if not found
+    if no default is given, variable is considered required and must be set
+    if not, print the required var and fail the program
 
-    if not url:
-        print("URL required")
+    args:
+        variable (str): environment variable to resolve
+        default (str): default value for variable if not found
+
+    returns:
+        str: the value of the variable
+    """
+
+    value = getenv(variable, default)
+    if not value:
+        # if we are missing a PLUGIN_ var, ask the user for the expected setting
+        stripped_variable = variable if "PLUGIN_" not in variable else variable[7:]
+        print(f"{stripped_variable} required")
         exit(1)
+
+    return value
+
+
+def main():
+    url = check_env("PLUGIN_URL")
 
     resp = get(url)
 
